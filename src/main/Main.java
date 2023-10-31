3e49b4d72b38d7ff20c91757cccf2747d64c57d0
naicon <naicon@localhost>
2023-10-31 15:09:25 +0700

    SearchFlie

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import controller.SearchFileProgram;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        new SearchFileProgram().run();
+    }
+    
+}
